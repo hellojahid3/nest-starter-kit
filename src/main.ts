1aabb9e8388af983d5867b1c67eb82ccd0ce56e2
Md. Jahid Hasan <78964030+hellojahid3@users.noreply.github.com>
2026-02-19 14:10:56 +0600

    Remove CORS configuration in bootstrap function
    Removed CORS configuration from the bootstrap function.

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,6 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: true,
     bodyParser: false,
     bufferLogs: true,
   });
